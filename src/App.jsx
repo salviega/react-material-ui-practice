a0a2a9eb79d7cbca3600f668b9c46f3203ee9f99
salviega <salviega6@gmail.com>
2022-12-26 07:15:40 -0500

    [FEAT] Was added syles in the searcher

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,26 @@
-import React from "react";
+import React from 'react';
+import { Container } from '@mui/material';
+import { Searcher } from './containers/Searcher';
 
 const App = () => {
-  return(
-    <div>Soy un platzinauta</div>
-  )
+  const [inputUser, setInputUser] = React.useState('octocat');
+  const [useState, setUserState] = React.useState('inputUser');
+  return (
+    <Container
+      sx={{
+        marginTop: '40px',
+        width: '80wh',
+        height: '800px',
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        borderRadiuus: '16px',
+        background: 'whitesmoke',
+      }}
+    >
+      <Searcher inputUser={inputUser} setInputUser={setInputUser} />
+    </Container>
+  );
 };
 
 export default App;
